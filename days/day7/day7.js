2dcb9923c76fb4e0a2fc7d6cdfbd8d81466dfdd8
vaser888 <52551019+vaser888@users.noreply.github.com>
2022-12-08 02:07:39 -0500

    temp

--- days/day7/day7.js
+++ days/day7/day7.js
@@ -6,7 +6,60 @@ fetch('/days/day7/data.txt') // get the data from text file.
 })
 
 function main(data){
-    console.log(data);
+
+  function separateLines(textFile) {
+    let lineArray = textFile.split("\r\n");
+    return lineArray;
+  }
+
+  function makeInstructionsArray(array){
+    let instructionArray = [];
+    for (i = 0; i < array.length; i++){
+      let str = array[i];
+      instructionArray.push(str.split(" "));
+    }
+    return instructionArray;
+  }
+
+  function creatCompleteDirectoryArray(array){
+    let dirPointer = 0;
+    let treeArray = [];
+    for (i = 0; i < array.length; i++){
+      if(array[i][0] === "$"){
+        
+        if ( array[i][1] === "cd"){
+          switch(array[i][2]){
+            case "/":
+              dirPointer = 0;
+              break;
+            case "..":
+              dirPointer = dirPointer - 1;
+
+              break;
+            default:
+              //directory name
+              dirPointer = dirPointer + 1;
+              break;
+          }
+          console.log(dirPointer, i) 
+        }
+        if (array[i][1] === "ls"){
+          console.log("start of directory")
+        }
+      }
+      if (array[i][0] === "dir"){
+        console.log(array[i][1], "hey");        
+      }
+      if (array[i][0] != "dir" && array[i][0] !== "$"){
+        console.log(array[i][0], "bop", i)
+      }
+    }
+  }
+
+  let instructions = separateLines(data);
+  let instructionsArray = makeInstructionsArray(instructions);
+  let fullDirectory = creatCompleteDirectoryArray(instructionsArray);
+  console.log(instructionsArray);
 
     document.getElementById("solutionOutput").innerText= "Part 1: " + "\nPart 2: ";
 }
--- days/day8/day8.js
+++ days/day8/day8.js
@@ -6,7 +6,72 @@ fetch('/days/day8/data.txt') // get the data from text file.
 })
 
 function main(data){
-    console.log(data);
 
-    document.getElementById("solutionOutput").innerText= "Part 1: " + "\nPart 2: ";
+  function separateLines(textFile) {
+    let lineArray = textFile.split("\r\n");
+    return lineArray;
+  }
+
+  function splitArray(array, splitString) {
+    let splitGroup = [];
+    for (i = 0; i < array.length; i++){
+      splitGroup.push(array[i].split(splitString));
+    }
+    return splitGroup;
+  }
+
+  function checkFromleft(treeValue1, x1, theArray){
+    let tallestTree = 0;
+    for (i = 0; i < x1; i++){
+      if (tallestTree < theArray[i]){
+        tallestTree = theArray[i];
+      }
+      if (treeValue1 > tallestTree && x1 === i + 1){
+        return true;
+      }
+    }
+    return false;
+  }
+
+function checkFromRight(treeValue1, x1, theArray){
+    let tallestTree = 0;
+    for (i = theArray.length-1; i > x1; i--){
+      if (tallestTree < theArray[i]){
+        tallestTree = theArray[i];
+      }
+      if (treeValue1 > tallestTree && x1 === i - 1){
+        return true;
+      }
+    }
+    return false;
+  }
+
+
+  function partOne(array){
+    let totalVisibleTrees = array[0].length + array.length - 1 + array[0].length - 2 + array.length - 1
+    
+    for (y = 1; y < array.length - 1; y++){
+      for (x = 1; x < array[y].length - 1; x++){
+        let treeValue = array[y][x];
+        let visibleLeft = checkFromleft(treeValue, x, array[y]);
+        let visibleRight = checkFromRight(treeValue, x, array[y]);
+
+        if (visibleLeft == true || visibleRight == true){
+          totalVisibleTrees += 1
+        }
+        console.log(visibleRight);
+      }
+      
+      //console.log(array[y][x], x,y)
+    }
+    console.log(totalVisibleTrees);
+  }
+  
+
+  let array1 = separateLines(data);
+  array1 = splitArray(array1, "");
+ 
+  part1 = partOne(array1);
+  console.log(array1)
+  document.getElementById("solutionOutput").innerText= "Part 1: " + "\nPart 2: ";
 }
