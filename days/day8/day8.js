8bdef321ba2fa5a869533ec2f9ce1350deb7146d
vaser888 <52551019+vaser888@users.noreply.github.com>
2022-12-09 01:41:50 -0500

    day 8 part1 temp day 9

--- days/day8/day8.js
+++ days/day8/day8.js
@@ -8,7 +8,7 @@ fetch('/days/day8/data.txt') // get the data from text file.
 function main(data){
   console.log(data)
   function separateLines(textFile) {
-    let lineArray = textFile.split("\n");
+    let lineArray = textFile.split("\r\n");
     return lineArray;
   }
 
@@ -46,32 +46,31 @@ function checkFromRight(treeValue1, x1, theArray){
     return false;
   }
 
-  function checkFromTop(treeValue1, y1, x1 ,fullArray){
+  function checkFromTop(treeValue1, x1, y1 ,fullArray){
     let tallestTree = 0;
-    for (i = 0; i < y1; i++){
-      if (tallestTree < fullArray[i][x1]){
-        tallestTree = fullArray[i][x1];
+    for (counter = 0; counter < y1; counter++){
+      if (tallestTree < fullArray[counter][x1]){
+        tallestTree = fullArray[counter][x1];
       }
-      if (treeValue1 > tallestTree && y1 === i + 1){
+      if (treeValue1 > tallestTree && y1 === counter + 1){
         return true;
       }
     }
     return false;
   }
 
-  function checkFromBottom(treeValue1, y1, x1 ,fullArray){
+  function checkFromBottom(treeValue1, x1, y1 ,fullArray){
     let tallestTree = 0;
-    for (i = fullArray.length-1; i > y1; i--){
-      if (tallestTree < fullArray[i][x1]){
-        tallestTree = fullArray[i][x1];
+    for (counter = fullArray.length-1; counter > y1; counter--){
+      if (tallestTree < fullArray[counter][x1]){
+        tallestTree = fullArray[counter][x1];
       }
-      if (treeValue1 > tallestTree && y1 === i - 1){
+      if (treeValue1 > tallestTree && y1 === counter - 1){
         return true;
       }
     }
     return false;
   }
-
 
   function partOne(array){
     let totalVisibleTrees = array[0].length + array.length - 1 + array[0].length - 2 + array.length - 1
@@ -86,21 +85,67 @@ function checkFromRight(treeValue1, x1, theArray){
 
         if (visibleLeft == true || visibleRight == true || visibleTop == true || visibleBottom == true){
           totalVisibleTrees += 1
-          console.log(treeValue, treeValue, x,y, visibleLeft, visibleRight, visibleTop, visibleBottom)
         }
       }
-      
-      
     }
-    console.log(totalVisibleTrees);
     return totalVisibleTrees;
   }
   
+  function getDistanceLeft(x1, y1, treeValue1, fullArray){
+    let distance = 0;
+    for(count = x1-1; count >= 0; count--){
+      //console.log("x:"+x1,"y:"+y1)
+      //console.log(count, fullArray[y1][count], treeValue1)
+      if (treeValue1 > fullArray[y1][count]){
+        distance = distance + 1;
+      }
+      if (treeValue1 <= fullArray[y1][count]){
+        distance = distance + 1;
+        //console.log("dis: "+distance);
+        return distance;
+      }
+    }
+    //console.log("dis: "+distance);
+    return distance;
+  }
+  function getDistanceRight(x1, y1, treeValue1, fullArray){
+    let distance = 0;
+    console.log(x1, y1, fullArray[y1].length)
+    for(count = x1+1; count >= fullArray[y1].length; count++){
+      console.log(count);
+      //console.log("x:"+x1,"y:"+y1)
+      //console.log(count, fullArray[y1][count], treeValue1)
+      if (treeValue1 > fullArray[y1][count]){
+        distance = distance + 1;
+      }
+      if (treeValue1 <= fullArray[y1][count]){
+        distance = distance + 1;
+        //console.log("dis: "+distance);
+        return distance;
+      }
+    }
+    //console.log("dis: "+distance);
+    return distance;
+
+  }
+
+  function partTwo(array){
+    for (y = 1; y < array.length - 1; y++){
+      for (x = 1; x < array[y].length - 1; x++){
+        let treeValue = array[y][x];
+        let distanceLeft = getDistanceLeft(x, y, treeValue, array);
+        let distanceRight = getDistanceRight(x, y, treeValue, array);
+        //console.log(treeValue)
+      }
+    }
+  }
 
   let array1 = separateLines(data);
   array1 = splitArray(array1, "");
  
   part1 = partOne(array1);
+
+  part2 = partTwo(array1);
 
   document.getElementById("solutionOutput").innerText= "Part 1: " + part1 + "\nPart 2: ";
 }
--- days/day9/day9.js
+++ days/day9/day9.js
@@ -6,7 +6,86 @@ fetch('/days/day9/data.txt') // get the data from text file.
 })
 
 function main(data){
-    console.log(data);
 
-    document.getElementById("solutionOutput").innerText= "Part 1: " + "\nPart 2: ";
+  function separateLines(textFile) {
+    let lineArray = textFile.split("\r\n");
+    return lineArray;
+  }
+  function splitArray(array, splitString) {
+    let splitGroup = [];
+    for (i = 0; i < array.length; i++){
+      splitGroup.push(array[i].split(splitString));
+    }
+    return splitGroup;
+  }
+
+  function calculateDifference(val1, val2){
+    return val1 - val2;
+  }
+  
+  function partOne(array){
+    let head = [0,0]; // x y
+    let tail = [0,0]; // x y
+
+    for(instruction = 0; instruction < array.length; instruction++){
+      let direction = array[instruction][0];
+      let distance = parseInt(array[instruction][1]);
+      let HX = head[0];
+      let HY = head[1];
+      let TX = tail[0];
+      let TY = tail[1];
+      switch(direction){
+        case "R":
+          for(i = HX; i <= distance + HX; i++){
+            head[0] = i;
+            let res = calculateDifference(head[0], tail[0])
+            //console.log(res)
+            if(res >1){
+              tail[0] +=1
+              if(tail[1] != head[1]){
+                tail[1] = head[1];
+
+              }
+            }
+
+            console.log(head, tail)
+          }
+
+        break;
+        case "L":
+          for(i = HX; i >= HX - distance; i--){
+            head[0] = i;
+
+          }
+
+        break;
+        case "U":
+          for(i = HY; i <= distance + HY; i++){
+            head[1] = i;
+
+          }
+
+        break;
+
+        case "D":
+          for(i = HY; i >= HY - distance; i--){
+            head[1] = i;
+
+          }
+
+        break;
+      }
+      
+
+    }
+
+
+  }
+  
+  let array1 = separateLines(data);
+  array1 = splitArray(array1," ");
+  console.log(array1)
+  let something = partOne(array1)
+
+  document.getElementById("solutionOutput").innerText= "Part 1: " + "\nPart 2: ";
 }
